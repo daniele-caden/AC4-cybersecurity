58eae7f12d7d59a96609a5220a45339fafb93a6c
Daniele Caden <contato.sophiebc@gmail.com>
2020-10-25 18:24:52 -0300

    Programa Fibonnacci

---
+++ fibos.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def fibonacci ():
+    x = 0
+    y = 1
+    contagem = 1
+    limite = 100
+    resultado = "0"
+    while ( contagem < limite):
+        z = x + y
+        resultado += "," +z
+        y = z 
+        x = y
+        contagem += 1
+    return resultado
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
